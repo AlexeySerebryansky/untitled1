e357ecd6084877450292caecada24b94d69bab17
Alexet Serebryanskiy <puma3333333333@gmail.com>
2022-01-16 19:13:50 +0200

    Initial commit

--- src/com/hillel/homework/lesson10/NegativeNumbers.java
+++ src/com/hillel/homework/lesson10/NegativeNumbers.java
@@ -8,21 +8,25 @@ import java.io.InputStreamReader;
 public class NegativeNumbers {
     public static void main(String[] args) throws IOException {
         System.out.println("enter size your matrix");
+
         System.out.println("enter how many line: ");
         int line = reader();
         System.out.println("enter how many column: ");
         int column = reader();
+
         int[][] matrix = new int[line][column];
+
         fillMatrix(matrix);
 
         systemOutMatrix(matrix);
 
         check(matrix);
 
-
     }
 
+
     private static void fillMatrix(int[][] matrix) throws IOException {
+
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.println("enter number: " + (i + 1) + "  " + (j + 1) + ";  ");
@@ -31,7 +35,9 @@ public class NegativeNumbers {
         }
     }
 
+
     private static void systemOutMatrix(int[][] matrix) {
+
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print("   " + matrix[i][j]);
@@ -43,7 +49,6 @@ public class NegativeNumbers {
 
     public static void check(int[][] matrix) {
 
-
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] < 0) {
@@ -54,7 +59,9 @@ public class NegativeNumbers {
         System.out.println();
     }
 
+
     private static int reader() throws IOException {
+
         BufferedReader READER = new BufferedReader((new InputStreamReader(System.in)));
         return Integer.parseInt(READER.readLine());
     }
