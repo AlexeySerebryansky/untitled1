720669ac284d99184e379993a4594eb650a8ae88
Alexet Serebryanskiy <puma3333333333@gmail.com>
2022-01-30 00:18:21 +0200

    Initial commit

--- src/com/hillel/homework/lesson6/randomNumbers.java
+++ src/com/hillel/homework/lesson6/randomNumbers.java
@@ -3,34 +3,38 @@ package com.hillel.homework.lesson6;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class randomNumbers {
     public static void main(String[] args) throws IOException {
-        BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("the largest of them: " + OperationWithArrayNumbers());
+
+    }
 
 
-        double firstNumber;
-        double secondNumbers;
-        double thirdNumbers;
-
-        System.out.println("Enter value for first number");
-        firstNumber = Integer.parseInt((READER.readLine()));
-        System.out.println("Enter value for second number");
-        secondNumbers = Integer.parseInt((READER.readLine()));
-        System.out.println("Enter value for third number");
-        thirdNumbers = Integer.parseInt((READER.readLine()));
-
-
-        if (firstNumber > secondNumbers && firstNumber > thirdNumbers) {
-            System.out.println(firstNumber);
-        }
-        if (secondNumbers > firstNumber && secondNumbers > thirdNumbers) {
-            System.out.println(secondNumbers);
-        }
-        if (thirdNumbers > firstNumber && thirdNumbers > secondNumbers) {
-            System.out.println(thirdNumbers);
+    private static Integer OperationWithArrayNumbers() throws IOException {
+        List<Integer> numbers = new ArrayList<>();
+        while (true) {
+            System.out.println("fill, if you want STOP - press '0' ");
+            int input = reader();
+            if (input == (0)) {
+                break;
+            }
+            numbers.add(input);
         }
 
+        System.out.println("your array: " + Arrays.toString(numbers.toArray(new Integer[0])));
 
+        return Collections.max(numbers);
+    }
+
+
+    private static int reader() throws IOException {
+        BufferedReader READER = new BufferedReader((new InputStreamReader(System.in)));
+        return Integer.parseInt(READER.readLine());
     }
 }
